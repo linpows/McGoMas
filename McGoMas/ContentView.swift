0416a13559ff3e03a44ff45e8f7a3816837af348
kayla20 <kayla20@vt.edu>
2020-03-03 12:47:59 -0500

    Initial Commit

---
+++ McGoMas/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  McGoMas
+//
+//  Created by Mikayla Richardson on 3/3/20.
+//  Copyright © 2020 Capstone. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
