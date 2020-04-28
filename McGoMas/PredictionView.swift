24d853b863b4e9c3f02a41c938c50e28c9cfad02
Nick Geyer <nickgeyer@Lauras-MacBook-Air-2.local>
2020-04-28 07:55:36 -0400

    Improvements to Traffic view

--- McGoMas/PredictionView.swift
+++ McGoMas/PredictionView.swift
@@ -13,29 +13,51 @@ struct PredictionView: View {
     @State private var dateToDisplay = minPredictionDate
     
     func getPredictionBar(pred: Prediction, currentDate: Date) -> some View {
-        return AnyView(HStack {
+        return AnyView(VStack(alignment: .center) {
             if (Calendar.current.isDate(pred.dateTime, inSameDayAs:currentDate) == true) {
-                Text("\(predictionTimeFormatter.string(from: pred.dateTime))")
-                Rectangle().fill(Color.blue).frame(width: CGFloat(pred.prediction) / 3, height: 10)
-                Text(" \(pred.prediction)")
+                ZStack {
+                    Rectangle().fill(Color.orange).frame(width: 13, height: CGFloat(pred.prediction) / 2)
+                    if pred.prediction > 30 {
+                        Text("\(pred.prediction)").font(.system(size: 7)).rotationEffect(.degrees(90)).foregroundColor(.white).fixedSize(horizontal: true, vertical: false)
+                    }
+                }
+                Text("\(predictionTimeFormatter.string(from: pred.dateTime))").font(.system(size: 7)).rotationEffect(.degrees(90)).fixedSize(horizontal: true, vertical: false)
             }
         })
     }
     
     var body: some View {
-        ScrollView {
-            VStack (alignment: .leading) {
-                DatePicker(selection: $dateToDisplay, in:minPredictionDate...maxPredictionDate, displayedComponents: .date) {
-                    Text("")
-                }
-                
-                Text("Predicted occupancy on \(dateToDisplay, formatter: predictionDateFormatter)")
+        VStack (alignment: .center){
+            HStack(){
+                // Page title
+                Text("Traffic")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .padding(.top, 15.0)
+                    .padding(.bottom, 20.0)
+            }
+            
+            Divider()
+            
+            DatePicker("", selection: $dateToDisplay, in:minPredictionDate...maxPredictionDate, displayedComponents: .date)
+            
+            Divider()
+            
+            Spacer()
+            
+            HStack(alignment: .bottom) {
                 ForEach(predictions, id: \.self) { pred in
                     self.getPredictionBar(pred: pred, currentDate: self.dateToDisplay)
                 }
-            }.padding(.leading, 10)
+            }
+            
+            Divider()
+            
+            Text("Time of day").padding(.bottom, 30)
+            
         }
     }
+    
 }
 struct PredictionView_Previews: PreviewProvider {
     static var previews: some View {
